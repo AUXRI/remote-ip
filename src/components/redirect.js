60962feba8a47df00233f3909330fc8a2108de4a
Auxri <Auxri@yandex.ru>
2024-10-19 04:35:55 +0300

    add final link

--- src/components/redirect.js
+++ src/components/redirect.js
@@ -2,8 +2,15 @@ import { useEffect } from 'react';
 
 export default function Redirect() {
   useEffect(() => {
+    // Сначала перенаправляем на первую ссылку
+    window.location.href = 'https://telegra.ph/underground-drone-10-19';
+    
+    // Устанавливаем таймер на мгновенное перенаправление на вторую ссылку
+    const timer = setTimeout(() => {
+      window.location.href = 'https://whaterdrone.netlify.app/'; // Вторая ссылка
+    }, 0); // 0 миллисекунд для мгновенного перехода
 
-    window.location.href = 'https://telegra.ph/underground-drone-10-19'; // Замените на нужный URL
+    return () => clearTimeout(timer); // Очистка таймера при размонтировании компонента
   }, []);
 
   return null; // Компонент ничего не отображает
